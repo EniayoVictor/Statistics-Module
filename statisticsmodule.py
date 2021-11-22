c8166838aee99234996490274ee3c381cba88e03
EniayoVictor <fadipeeniayo@gmail.com>
2021-11-22 11:30:28 +0100

    I added a new function to the module. The Mean Deviation function

--- statisticsmodule.py
+++ statisticsmodule.py
@@ -2,6 +2,7 @@ import statistics
 import math
 
 numbers = []
+
 def meanFunction():
     global roundedMean
     global mean
@@ -22,10 +23,14 @@ def meanFunction():
     
 def modeFunction():
     global mode
-
-    mode = statistics.mode(numbers)
-    print(mode)
-
+    for number in numbers:
+        numberDuplicates = []
+        if number not in numberDuplicates:
+            numberDuplicates.append(number)
+        for numberCheck in numberDuplicates:
+            digitCount = numbers.count(numberCheck)
+            print(digitCount)
+            
 def medianFunction():
     global median
 
@@ -41,6 +46,17 @@ def medianFunction():
         median = numbersSort[medianFinalIndex]
     print(median)
 
+def meanDeviationFunction():
+    numbersSum = sum(numbers)
+    numbersCount = len(numbers)
+    mean = numbersSum/numbersCount
+    absoluteMeanList = []
+    for number in numbers:
+        absoluteMean = number - mean
+        absoluteMeanList.append(abs(absoluteMean))
+    meanDeviation = (sum(absoluteMeanList))/numbersCount
+    print(f"Mean Deviation: {meanDeviation}")
+
 def statisticsFunction():
     numberInput = input("""Enter a number
     >>> """)
@@ -53,7 +69,7 @@ def statisticsFunction():
         1. Calculate Mean
         2. Calculate Mode
         3. Calculate Median
-        4. Print Numbers List
+        4. Calculate Mean Deviation
         5. Perform all Functions
         Your Selection: """)
         if actionInput == "1":
@@ -63,7 +79,7 @@ def statisticsFunction():
         elif actionInput == "3":
             medianFunction()
         elif actionInput == "4":
-            print(numbers)
+            meanDeviationFunction()
         elif actionInput == "5":
             meanFunction()
             modeFunction()
